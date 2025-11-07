ceb0322b43ec23bc43decd6827f5b105bec1d709
elvisarrudaestudos-spec <elvisarrudaestudos@gmail.com>
2025-11-07 14:32:56 +0000

    Digitei pseudocodigo

---
+++ PROJETOS/Pseudocódigo/pseudocodigo.c
@@ -0,0 +1,9 @@
+seria o uso de uma linguagem sem sintaxe ou código!
+Início
+    Solicitar ao usuário um número
+    Receber o número fornecido
+    Se o número for divisível por 2 então
+        Exibir "O número é par"
+    Senão
+        Exibir "O número é ímpar"
+Fim
