a2dab530ae1f821fdc9b6d1e417fb82888c6816b
elvisarrudaestudos-spec <elvisarrudaestudos@gmail.com>
2025-11-07 18:50:14 +0000

    digitei pos e pre incremento/decremento

--- PROJETOS/Operadores Matematicos/operadoresdeincrementodecrimento.c
+++ PROJETOS/Operadores Matematicos/operadoresdeincrementodecrimento.c
@@ -11,28 +11,33 @@ int main() {
     Pós-Decrimento a--
     */
 
-    int numero1= 1;
+    int numero1= 1, resultado;
     printf("Antes do incremento: %d\n", numero1);
-    numero1++;
-    printf("Apos o incremento: %d\n", numero1);
-
-    printf("Antes do decremento: %d\n", numero1);
-    numero1--;
-    printf("Apos o decremento: %d\n", numero1);
+    resultado = numero1++;
+    printf("Apos Pós-incremento - Numero 1: %d - resultado %d\n", numero1, resultado);
+    resultado = ++numero1;
+    printf("Apos Pré-incremento - Numero 1: %d - resultado %d\n", numero1, resultado);
+    resultado = numero1--;
+    printf("Apos Pós-decremento - Numero 1: %d - resultado %d\n", numero1, resultado);
+    resultado = --numero1;
+    printf("Apos Pré-decremento - Numero 1: %d - resultado %d\n", numero1, resultado);
+    //printf("Antes do decremento: %d\n", numero1);
+    //numero1--;
+    //printf("Apos o decremento: %d\n", numero1);
     
     //teria o mesmo resultado se
-    int numero1= 1;
-    printf("Antes do incremento: %d\n", numero1);
-    numero1= numero1 + 1;
-    printf("Apos o incremento: %d\n", numero1);
+    //int numero1= 1;
+    //printf("Antes do incremento: %d\n", numero1);
+    //numero1= numero1 + 1;
+    //printf("Apos o incremento: %d\n", numero1);
 
-    printf("Antes do decremento: %d\n", numero1);
-    numero1= numero1 - 1;
-    printf("Apos o decremento: %d\n", numero1);
+    //printf("Antes do decremento: %d\n", numero1);
+    //numero1= numero1 - 1;
+    //printf("Apos o decremento: %d\n", numero1);
 
     // com operadores aritméticos
     
-    printf("Antes do incremento: %d\n", numero1);
-    numero1 += 1;
+    //printf("Antes do incremento: %d\n", numero1);
+    //numero1 += 1;
 
 }
