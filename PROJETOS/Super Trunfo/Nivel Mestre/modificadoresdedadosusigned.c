b79f36605819d8f259ec25bcf15c628f0d7b2594
elvisarrudaestudos-spec <elvisarrudaestudos@gmail.com>
2025-11-11 12:33:22 +0000

    Digitei a observação

--- PROJETOS/Super Trunfo/Nivel Mestre/modificadoresdedadosusigned.c
+++ PROJETOS/Super Trunfo/Nivel Mestre/modificadoresdedadosusigned.c
@@ -1,4 +1,5 @@
 // a variável "int" trabalha tanto com valores positivos/ negativos ex. -2.147.148,648 e 2.147.148,648 bilhões;
+// obs. caso ele ultrapasse o nº de 2.147.148,648, ele retorna para o nº negativo ex. 3000000000= -1294967296;
 // já a variável "unsigned int" trabalha apenas com valores positivos, porém em uma quantidade maior ex. 0 a 4.294.967,295 bilhões;
 // já a "char" vai de -128 a + 127;
 // já a "unsigned int" 0 a 255;
