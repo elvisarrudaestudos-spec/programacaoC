88bb3c796b9c1a6478897559dd8d2d1869746f4f
elvisarrudaestudos-spec <elvisarrudaestudos@gmail.com>
2025-11-11 12:27:32 +0000

    Digitei unsign

---
+++ PROJETOS/Super Trunfo/Nivel Mestre/modificadoresdedadosusigned.c
@@ -0,0 +1,17 @@
+// a variável "int" trabalha tanto com valores positivos/ negativos ex. -2.147.148,648 e 2.147.148,648 bilhões;
+// já a variável "unsigned int" trabalha apenas com valores positivos, porém em uma quantidade maior ex. 0 a 4.294.967,295 bilhões;
+// já a "char" vai de -128 a + 127;
+// já a "unsigned int" 0 a 255;
+// ou seja o unsigned dobra o valor de forma positiva;
+
+#include <stdio.h>
+ 
+int main() {
+    int signedNumber = 3000000000; // Este valor excede o limite de um int normal
+    unsigned int unsignedNumber = 3000000000;
+ 
+    printf("Número com sinal: %d\n", signedNumber);
+    printf("Número sem sinal: %u\n", unsignedNumber);
+ 
+    return 0;
+}
