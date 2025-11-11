6f493c4274268ed78a33e928f94a6e049c067809
elvisarrudaestudos-spec <elvisarrudaestudos@gmail.com>
2025-11-11 14:19:55 +0000

    Fim da lição long

--- PROJETOS/Super Trunfo/Nivel Mestre/modificadoresdedadoslong.c
+++ PROJETOS/Super Trunfo/Nivel Mestre/modificadoresdedadoslong.c
@@ -12,13 +12,21 @@
 int main() {
     int numeroNormal = 2147483647; // Valor máximo de int
     long int numeroGrande = 2147483647;
- 
+    double NumeroPreciso = 3.141592653589793;
+    long double NumeroMuitoPreciso = 3.14159265358979323846;
+    unsigned long int NumeroPositivoGrande = 4000000000;
+    
     printf("Número regular (int): %d\n", numeroNormal);
     printf("Número grande (long int): %ld\n", numeroGrande);
  
     numeroGrande = 2147483648; // Valor maior que o máximo de int
     printf("Número grande atualizado (long int): %ld\n", numeroGrande); // %ld= nº grande, %lld double
- 
+    
+    printf("Número preciso (double): %.15f\n", NumeroPreciso);
+    printf("Número muito preciso (long double): %.21Lf\n", NumeroMuitoPreciso); // nem sempre ele dará um nº preciso, por isso tem que ter muito cuidado!
+
+    printf("Número positivo grande: %lu\n", NumeroPositivoGrande);
+
     return 0;
 
 }
