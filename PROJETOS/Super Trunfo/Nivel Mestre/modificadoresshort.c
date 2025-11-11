e52ef3ff248430b156182da52ba1ee4c81773779
elvisarrudaestudos-spec <elvisarrudaestudos@gmail.com>
2025-11-11 14:44:25 +0000

    Digitei o short

---
+++ PROJETOS/Super Trunfo/Nivel Mestre/modificadoresshort.c
@@ -0,0 +1,22 @@
+// tem a função de trabalhar com nº menores;
+
+
+
+
+
+
+#include <stdio.h>
+ 
+int main() {
+    short int numeroPequeno = 32767; // Valor máximo do short
+    printf("Número Pequeno (short int): %d\n", numeroPequeno);
+    
+    numeroPequeno = 32768; // veja que aqui a dezena foi maior, ou seja, mostrou um valor negativo;
+    printf("Número Pequeno (short int): %d\n", numeroPequeno);
+
+    printf("\n *** tamanho das variáveis ***\n");
+    printf("Short int: %lu B - int: %lu B -long int: %lu B\n", sizeof(short int), sizeof(int), sizeof( long long int));
+    printf("float: %lu B - double: %lu B -long double: %lu B\n", sizeof(float), sizeof(double), sizeof( long double));
+    
+    return 0;
+}
