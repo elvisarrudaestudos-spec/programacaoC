230a5019533ec1409cd2b0c6b7bf5f721d1be19b
elvisarrudaestudos-spec <elvisarrudaestudos@gmail.com>
2025-11-12 11:57:39 +0000

    Finalizei o hora de codar

--- PROJETOS/Super Trunfo/Nivel Mestre/hora de codar.c
+++ PROJETOS/Super Trunfo/Nivel Mestre/hora de codar.c
@@ -20,23 +20,25 @@ unsigned int estoqueMinimoB= 2500;
 double ValorTotalA;
 double ValorTotalb;
 
+int resultadoA, resultadoB;
+
 //exibir as informações dos proidutos
 printf("Produto %s tem estoque %u e o valor unitário é R$ %.2f\n", produtoA, estoqueA, valorA);
 printf("Produto %s tem estoque %u e o valor unitário é R$ %.2f\n", produtoB, estoqueB, valorB);
 
 //comparações com o valor mínimo de estoque
 
+resultadoA = estoqueA > estoqueMinimoA;
+resultadoB = estoqueB > estoqueMinimoB;
+
+printf("O produto %s tem estoque mínimo %d\n", produtoA, resultadoA);
+printf("O produto %s tem estoque mínimo %d\n", produtoB, resultadoB);
 
 //comparações entre valores totais dos produtos
 
-
-
-
-
-
-
-
-
-
+printf("O valor total de A (R$ %.2f) é maior que o valor total de B (R$ %.2f)? %d\n", 
+estoqueA * valorA,
+estoqueB * valorB,                                        
+(estoqueA * valorA) > (estoqueB * valorA));
 
 }
