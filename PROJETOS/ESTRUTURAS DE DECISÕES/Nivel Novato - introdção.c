d27fe1cf08b7d35aed99fb60ab01a4299a0eb84a
elvisarrudaestudos-spec <elvisarrudaestudos@gmail.com>
2025-11-13 17:47:40 +0000

    commit

---
+++ PROJETOS/ESTRUTURAS DE DECISÕES/Nivel Novato - introdção.c
@@ -0,0 +1,10 @@
+// entrar no site portugol.dev pois ele trabalha em pseudocódigo;
+// para trabalhar com as estruturas de decisões deve ser usado com os operadores relacionais, pois eles comparam dois valores e retornam verdadeiro (true) ou falso (false).
+// ex: de operadores relacionais:
+    // > : Maior que
+    // < : Menor que
+    // >= : Maior ou igual a
+    // <= : Menor ou igual a
+    // == : Igual a
+    // != : Diferente de
+    
