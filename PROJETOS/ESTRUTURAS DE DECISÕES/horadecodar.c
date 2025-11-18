fa0d722743a9ccace3c136b20ba5c11a143fe0ea
elvisarrudaestudos-spec <elvisarrudaestudos@gmail.com>
2025-11-17 21:04:07 -0300

    Hora de codar estruturas

--- PROJETOS/ESTRUTURAS DE DECISÕES/horadecodar.c
+++ PROJETOS/ESTRUTURAS DE DECISÕES/horadecodar.c
@@ -1,20 +1,34 @@
 #include <stdio.h>
 
 int main() {
-float 
+float temperatura, umidade;
+unsigned int estoque, estoqueminimo = 1000;
 
+    printf("Entre com a Temperatura.\n");
+    scanf("%f", &temperatura);
+    
+    printf("Entre com a Umidade.\n");
+    scanf ("%f", &umidade);
 
-if
+    printf("Entre com o Estoque.\n");
+    scanf("%u", &estoque);
 
-
-else
-
-
-
-
-
-
-return 0;
+    if(temperatura > 30){
+        printf("Temperatura esta alta.\n");
+    } else {
+        printf("Temperatura esta dentro dos parametros.\n");
+    }
+    if(umidade > 50){
+        printf("Umidade esta elevada.\n");
+    } else {
+        printf("Temperatura esta dentro dos parametros.\n");
+    }
+    if(estoque < estoqueminimo){
+        printf("Estoque abaixo do minimo.\n");
+    } else { 
+        printf("O estoque normal.\n");
+    }
+    return 0;
   
 }
 
